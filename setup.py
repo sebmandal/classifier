e6b3fd44fa0631f6fb51c890b889eecdb25149a6
Sebastian Mandal <sebastian@mandal.group>
2024-09-30 14:03:48 +0200

    chore: updates requirements and setup

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="sentiment-prediction",
+    name="digit-classifier",
     version="0.1",
-    description=
-    "Multilayer Perceptron Network for Digit Recognition using MNIST dataset.",
+    description="MLP Network for Digit Recognition using MNIST 784 dataset.",
     author="Sebastian Mandal",
-    author_email="sebastian@mandal.group",
-    packages=[""],
-    install_requires=[""],
+    author_email="sebastian.mandal@icloud.com",
+    packages=["module"],
+    install_requires=[
+        "numpy==1.24.2",
+        "scikit-learn==1.2.0",
+        "matplotlib==3.7.1",
+        "pandas==2.0.3",
+    ],
 )
